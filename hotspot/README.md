4eade10b85e48ce46a6adbcdfa2e805c0d8b7d0e
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-09-05 10:46:12 +0200

    Added a description

---
+++ hotspot/README.md
@@ -0,0 +1,5 @@
+# HotSpot
+
+The package provides an interface to
+[HotSpot](http://lava.cs.virginia.edu/HotSpot/),
+a thermal model and simulator used in computer-architecture studies.
---
+++ hotspot/main.go
@@ -0,0 +1,5 @@
+// Package hotspot provides an interface to HotSpot, a thermal model and
+// simulator used in computer-architecture studies.
+//
+// http://lava.cs.virginia.edu/HotSpot/
+package hotspot
