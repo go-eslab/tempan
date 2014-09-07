bd2e01b12619022f46cd489a93bfa15b603e0f52
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-09-07 15:30:49 +0200

    Added an extensive test for expint

--- expint/main_test.go
+++ expint/main_test.go
@@ -10,5 +10,15 @@ func TestNew(t *testing.T) {
 	s := New(findFixture("problem.json"))
 
 	assert.Equal(s.Cores, uint16(2), t)
-	assert.Equal(s.Nodes, uint16(2*4+12), t)
+	assert.Equal(s.Nodes, uint16(4*2+12), t)
+
+	assert.Equal(s.system.A, fixtureA, t)
+	assert.Equal(s.system.B, fixtureB, t)
+	assert.Equal(s.system.C, fixtureC, t)
+
+	assert.Equal(s.system.U, fixtureU, t)
+	assert.Equal(s.system.L, fixtureL, t)
+
+	assert.Equal(s.system.E, fixtureE, t)
+	assert.Equal(s.system.F, fixtureF, t)
 }
