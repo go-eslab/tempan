e83e9d2b3839fc5b17b300f7c681e3667226dc35
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-10-27 08:42:10 +0100

    Adjust the description of Config

--- config.go
+++ config.go
@@ -1,9 +1,9 @@
 package hotspot
 
-// Config is a configuration of the HotSpot model.
+// Config is a configuration of HotSpot.
 type Config struct {
 	// A floorplan of a multiprocessor system.
 	Floorplan string
-	// A native configuration file of the HotSpot model (hotspot.config).
+	// A native configuration file of HotSpot (hotspot.config).
 	Configuration string
 }
